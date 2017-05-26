070806273215e7a15d61453b491048804efb9f36
f-suzuki1231 <suzuki.f.aa@m.titech.ac.jp>
2017-05-26 16:18:22 +0900

    homework

--- app/src/main/java/jp/ac/titech/itpro/sdl/gles10ex/HexagonalPyramid.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/gles10ex/HexagonalPyramid.java
@@ -2,7 +2,7 @@ package jp.ac.titech.itpro.sdl.gles10ex;
 
 /**
  * Created by fuyu_suzuki on 2017/05/26.
- * 六角錐を追加（課題：立体図形を追加せよ）
+ * 課題：立体図形を追加せよ
  */
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
@@ -18,12 +18,7 @@ public class HexagonalPyramid implements SimpleRenderer.Obj {
     public HexagonalPyramid(float s, float x, float y, float z) {
         float[] vertices = {
                 //bottom
-                 0,    0.7f, 0,
-                -0.7f, 0.5f, 0,
-                -0.7f,-0.5f, 0,
-                 0,   -0.7f, 0,
-                 0.7f,-0.5f, 0,
-                 0.7f, 0.5f, 0,
+
 
 
                  0,    0.7f, 0,
@@ -48,7 +43,34 @@ public class HexagonalPyramid implements SimpleRenderer.Obj {
 
                  0.7f, 0.5f, 0,
                  0,       0, 1,
-                 0,    0.7f, 0
+                 0,    0.7f, 0,
+
+
+
+                0,    0.7f, 0,
+                0,       0, -1,//頂点
+                -0.7f, 0.5f, 0,
+
+                -0.7f, 0.5f, 0,
+                0,       0, -1,
+                -0.7f,-0.5f, 0,
+
+                -0.7f,-0.5f, 0,
+                0,       0, -1,
+                0,   -0.7f, 0,
+
+                0,   -0.7f, 0,
+                0,       0, -1,
+                0.7f,-0.5f, 0,
+
+                0.7f,-0.5f, 0,
+                0,       0, -1,
+                0.7f, 0.5f, 0,
+
+                0.7f, 0.5f, 0,
+                0,       0, -1,
+                0,    0.7f, 0
+
 
         };
         vbuf = ByteBuffer.allocateDirect(vertices.length * 4)
@@ -64,34 +86,54 @@ public class HexagonalPyramid implements SimpleRenderer.Obj {
     public void draw(GL10 gl) {
         gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
         gl.glVertexPointer(3, GL10.GL_FLOAT, 0, vbuf);
+        gl.glClearColor(1,0,0,1);
 
 
-
-       // bottom
-        gl.glNormal3f(0, 1, 1.5f);
-        gl.glDrawArrays(GL10.GL_TRIANGLE_FAN, 0, 6);
-
+       // top
 
         gl.glNormal3f(0.5f, 1, 0.5f);
-        gl.glDrawArrays(GL10.GL_TRIANGLES, 6, 3);
+        gl.glDrawArrays(GL10.GL_TRIANGLES, 0, 3);
 
 
         gl.glNormal3f(-0.5f, 0, 0.5f);
-        gl.glDrawArrays(GL10.GL_TRIANGLES, 9, 3);
+        gl.glDrawArrays(GL10.GL_TRIANGLES, 3, 3);
 
 
         gl.glNormal3f(1, 0, 0.8f);
-        gl.glDrawArrays(GL10.GL_TRIANGLES, 12, 3);
+        gl.glDrawArrays(GL10.GL_TRIANGLES, 6, 3);
 
 
         gl.glNormal3f(0.5f,-0.5f,0.5f);
-        gl.glDrawArrays(GL10.GL_TRIANGLES, 15, 3);
+        gl.glDrawArrays(GL10.GL_TRIANGLES, 9, 3);
 
         gl.glNormal3f(-0.5f, 0, 1);
-        gl.glDrawArrays(GL10.GL_TRIANGLES, 18, 3);
+        gl.glDrawArrays(GL10.GL_TRIANGLES, 12, 3);
 
         gl.glNormal3f(0,0,0.8f);
+        gl.glDrawArrays(GL10.GL_TRIANGLES, 15, 3);
+
+        //bottom
+
+        gl.glNormal3f(0.5f, 1, 0.5f);
+        gl.glDrawArrays(GL10.GL_TRIANGLES, 18, 3);
+
+
+        gl.glNormal3f(-0.5f, 0, 0.5f);
         gl.glDrawArrays(GL10.GL_TRIANGLES, 21, 3);
+
+
+        gl.glNormal3f(1, 0, 0.8f);
+        gl.glDrawArrays(GL10.GL_TRIANGLES, 24, 3);
+
+
+        gl.glNormal3f(0.5f,-0.5f,0.5f);
+        gl.glDrawArrays(GL10.GL_TRIANGLES, 27, 3);
+
+        gl.glNormal3f(-0.5f, 0, 1);
+        gl.glDrawArrays(GL10.GL_TRIANGLES, 30, 3);
+
+        gl.glNormal3f(0,0,0.8f);
+        gl.glDrawArrays(GL10.GL_TRIANGLES, 33, 3);
 
     }
 
